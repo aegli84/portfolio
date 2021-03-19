10c3a836590b0c1967c6cbca7e22ceab2e057010
Andreea Egli <64198045+aegli84@users.noreply.github.com>
2021-03-19 22:42:27 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
